c11b08b2c22634b7619912f6a5dcce6ff0bb82bf
mujtaba <chaoticworld685@hotmail.com>
2023-06-08 23:52:08 +0200

    add bulk requests and geo_point coordinates

--- elasticsearchIngestion/main.py
+++ elasticsearchIngestion/main.py
@@ -9,10 +9,13 @@ import tarfile
 load_dotenv()
 URL = os.getenv("URL")
 ABS_PATH = os.getenv("ABS_PATH")
-HEADERS = {'Content-Type': 'application/json', 'Authorization': os.getenv("API_KEY"), }
-AUTH = os.getenv("AUTH")
+HEADERS = {'Content-Type': 'application/json', }#'Authorization': os.getenv("API_KEY"), }
+AUTH = HTTPBasicAuth(os.getenv("USER"), os.getenv('PASSWORD'))
 VERIFY = os.getenv("VERIFY")
+BUFFER_SIZE = 1000
 
+#check if have to reindex data if memory runs out and elasticsearch crashes
+#word clouds for trending hash tags
 
 def sendPostRequest(request, data):
     if AUTH == "":
@@ -32,7 +35,7 @@ def deleteDoc(doc_id):
 
 def deleteAllDocs():
     data = "{ \"query\": { \"match_all\": {} } }"
-    sendPostRequest(URL[:-5] + "/_delete_by_query?conflicts=proceed", data)
+    sendPostRequest(URL[:-6] + "/_delete_by_query?conflicts=proceed", data)
 
 
 def extract():
@@ -47,8 +50,10 @@ def extract():
 
 
 def index():
-    # count=0
     min_entries = os.listdir(ABS_PATH)  # list of all minute folder names
+    docs = []
+    bulk_data = ""
+    count = 0
     for min_file in min_entries:  # iterate over all minute files
         min_path = ABS_PATH + min_file + "/"
         sec_entries = os.listdir(min_path)  # list of all second folder names
@@ -67,33 +72,40 @@ def index():
                             place_id = doc["geo"]["place_id"]
                             for place in places:
                                 if place["id"] == place_id:
-                                    # count = count + 1
-                                    # print(count)
                                     doc["place"] = place
                                     bbox = doc["place"]["geo"]["bbox"]
-                                    # if count<7:
-                                    #     doc["place"]["geo"]["bbox"] = {"type": "envelope",
-                                    #                                    "coordinates": [[-67.1425907, 10.6059297],[-66.9123794, 10.5178096]]}
-                                    #
-                                    # else:
-                                    doc["place"]["geo"]["bbox"] = {"type": "envelope", "coordinates": [[float(bbox[0]), float(bbox[3])], [float(bbox[2]), float(bbox[1])]]}  #envelope
-                                        # print(bbox[2], bbox[3], bbox[0], bbox[1])
-                                        # print(doc["place"]["country"])
-                                        # [[bbox[2], bbox[3]], [bbox[0], bbox[1]]]}
+                                    doc["place"]["geo"] = doc["place"]["geo"] = \
+                                        {"bbox": {"type": "envelope", "coordinates": [[float(bbox[0]), float(bbox[3])], [float(bbox[2]), float(bbox[1])]]},
+                                         "center": {"type": "point", "coordinates": [(float(bbox[0])+float(bbox[2]))/2, (float(bbox[3])+ float(bbox[1]))/2]}}
                         except:
                             pass
-                        # doc["place"] = {"geo": {"bbox": {"type" : "envelope", "coordinates":[[-77.03653, 38.897676], [-77.009051, 38.889939]]}, "type":"feature"}, "country":"Spanien"}
                         doc["timestamp_second"] = int(sec_file[-7:-5])
                         doc["timestamp_minute"] = int(min_file)
-                        r = requests.post(URL, headers=HEADERS, json=doc, )
-                        print(r.text)
+                        docs.append(doc)
+                        count = count+1
+                        # Bulk index the data
+                        if count > BUFFER_SIZE:
+                            for i, data in enumerate(docs):
+                                bulk_data += json.dumps({"index": {"_id": i}}) + "\n"
+                                # add the document data
+                                bulk_data += json.dumps(data) + "\n"
+                            r = requests.post(URL, headers=HEADERS, data=bulk_data, auth=AUTH, verify=VERIFY )
+                            # reset the bulk_data and docs variables
+                            bulk_data = ""
+                            docs = []
+                            count = 0
+                            print(r.content)
                         # break
-                    # data = {'data': data, "second_timestamp": int(sec_file[-7:-5]), 'minute_timestamp': int(min_file)}
-                    # r = requests.post(URL, headers=HEADERS, json=data,)
-                    # print(r.text)
                     # break
         #     break
         # break
+    for i, data in enumerate(docs):
+        bulk_data += json.dumps({"index": {"_id": count + 1}}) + "\n"
+        # Add the document data
+        bulk_data += json.dumps(data) + "\n"
+    r = requests.post(URL, headers=HEADERS, data=bulk_data, auth=AUTH, verify=VERIFY)
+    # Reset the bulk_data variable
+    print(r.content)
 
 
 # deleteDoc("0K1zmYcBf-vfNQzqUNj1")
